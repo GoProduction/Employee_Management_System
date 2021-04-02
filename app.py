119b332bfb8fc69f6a2f675d383e0739b93a8a9e
Erick McCoy <goshenoaksproductions@gmail.com>
2021-04-01 20:14:39 -0400

    -minor adjustment to app

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    employees_controller.get_employees
+    employees_controller.get_employees()
     return render_template('index.html')
 
 @app.route("/dashboard")
 def dashboard():
     # this is a comment
     return render_template('dashboard.html')
-
 
 if __name__ == "__main__":
     app.run()
