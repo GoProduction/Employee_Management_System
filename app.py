d4f9294786ef9f3d0cf44e45f4aa4b1da8ffdd91
Francis Bui <francisvbui@gmail.com>
2021-04-01 21:40:15 -0400

    Added pages and editing the routing to those pages on app.py

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template
 import connection
-import cx_Oracle
+# import cx_Oracle
 from controllers import employees_controller
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     employees_controller.get_employees()
     return render_template('index.html')
 
-@app.route("/dashboard")
-def dashboard():
-    # this is a comment
-    return render_template('dashboard.html')
+
+@app.route("/department")
+def department():
+    return render_template('department.html')
+
+
+@app.route("/user-add")
+def user_add():
+    return render_template('user-add.html')
+
+
+@app.route("/user-remove")
+def user_remove():
+    return render_template('user-remove.html')
+
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
 
 if __name__ == "__main__":
     app.run()
--- connection.py
+++ connection.py
@@ -8,7 +8,14 @@ db_connection_string = "tcps://adb.us-ashburn-1.oraclecloud.com:1522/sdxlk72ygu0
     
 # DB connector
 def connect_db():
-    conn = cx_Oracle.connect(db_username, db_password, db_connection_string)
-    return conn.cursor()
+        conn = cx_Oracle.connect(db_username, db_password, db_connection_string)
+        c = conn.cursor()
+        print('Successful connection')
+        c.execute("SELECT * FROM EMPLOYEES")
+
+        # test code to make sure this shit works
+        for row in c:
+            print('Employee First Name: ', row[2], 'Employee Last Name: ', row[3])
+        conn.close()
 
 
