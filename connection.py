6459bf3e17a6efb7476064db4b6c48abaf80d3d7
Erick McCoy <goshenoaksproductions@gmail.com>
2021-04-07 00:04:23 -0400

    Specified exception for connection method

--- connection.py
+++ connection.py
@@ -12,5 +12,5 @@ def connect_db():
         conn = cx_Oracle.connect(db_username, db_password, db_connection_string)
         print('Successful connection')
         return conn
-    except NetworkError:
+    except (RuntimeError, TypeError, NameError):
         print('Cannot connect to DB')
