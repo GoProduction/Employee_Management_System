cb398434567056110eecf3c71ad62d5b7900de07
Erick McCoy <goshenoaksproductions@gmail.com>
2021-04-22 11:39:44 -0400

    Created get_user functionality for backend

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,1 +1,19 @@
+from connection import connect_db
+import cx_Oracle
 
+
+def get_user(username, password):
+    found_user = []
+    sql = "select * from users where user_name = :username and user_pass = :password"
+    conn = connect_db()
+    c = conn.cursor()
+
+    c.execute(sql, [username, password])
+    for row in c:
+        found_user.append(row)
+
+    if(found_user == []):
+        return False
+    else:
+        print("User was found")
+        return True
