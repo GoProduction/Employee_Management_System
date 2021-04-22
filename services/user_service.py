635aeecd09d7538ce9cd54a9b322231d02d0903d
Erick McCoy <goshenoaksproductions@gmail.com>
2021-04-22 11:40:01 -0400

    Created validate_user for frontend logic

--- services/user_service.py
+++ services/user_service.py
@@ -1,1 +1,7 @@
+from flask import request
+from controllers import users_controller
 
+def validate_user():
+    username = request.form['username']
+    password = request.form['password']
+    return users_controller.get_user(username, password)
