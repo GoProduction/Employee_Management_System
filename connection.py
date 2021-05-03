000a3650471dfd48056cba55bba3eb4803924304
Francis Bui <francisvbui@gmail.com>
2021-05-02 20:24:39 -0400

    Melissa's changes to info

--- connection.py
+++ connection.py
@@ -9,13 +9,14 @@ db_username = "ADMIN"
 db_password = "nb9QRpFzHEQgbV3"
 db_connection_string = "tcps://adb.us-ashburn-1.oraclecloud.com:1522/sdxlk72ygu0rs6u_db202104011218_medium.adb.oraclecloud.com?wallet_location=network/wallet"
 
+# for macOS users
+if sys.platform.startswith("darwin"):
+    cx_Oracle.init_oracle_client(lib_dir=r"/Users/francisbui/Desktop/instantclient_19_8")
+
 
 # DB connector
 def connect_db():
     try:
-        # for macOS users
-        if sys.platform.startswith("darwin"):
-            cx_Oracle.init_oracle_client(lib_dir=r"/Users/francisbui/Desktop/instantclient_19_8")
 
         conn = cx_Oracle.connect(db_username, db_password, db_connection_string)
         print('Successful connection')
@@ -23,4 +24,3 @@ def connect_db():
     except (RuntimeError, TypeError, NameError):
         print('Cannot connect to DB')
 
-connect_db()
